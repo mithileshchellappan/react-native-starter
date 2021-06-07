60c5c67b50b8ea2aabee0e81c70601047bda0267
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-06-07 19:17:21 +0530

    init

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Appbar } from "react-native-paper";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
+
+  const handlePress = () => console.log('object')
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.wow}>
+      <Text numberOfLines={1}onPress={handlePress}>
+        Hellow World asdadsa asdads adas dd asd asdsad as asdas d asd as dasd
+        asd sada sd sad as dasd asd asd asdsadasdsa asd asd asd asdasd asd as
+        dsad a
+      </Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  wow: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "dodgerblue",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
