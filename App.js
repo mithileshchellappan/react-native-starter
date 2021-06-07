6a1bb127d8c37ef9f41ab461d59e541d242cbd78
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-06-07 20:07:56 +0530

    image and touchables

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Appbar } from "react-native-paper";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+
+} from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
-
-  const handlePress = () => console.log('object')
-
+  const handlePress = () => console.log("object");
+  console.log(require("./assets/icon.jpg"));
   return (
     <SafeAreaView style={styles.wow}>
-      <Text numberOfLines={1}onPress={handlePress}>
-        Hellow World asdadsa asdads adas dd asd asdsad as asdas d asd as dasd
-        asd sada sd sad as dasd asd asd asdsadasdsa asd asd asd asdasd asd as
-        dsad a
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hellow World
       </Text>
+      <TouchableHighlight onPress={handlePress}>
+        {/* <Image
+          fadeDuration={1000}
+          source={{
+            height: 600,
+            width: 300,
+            uri: "https://picsum.photos/600/00",
+          }}
+        /> */}
+        <View style={{width:200,height:70,backgroundColor:"dodgerblue"}}></View>
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
@@ -22,7 +38,7 @@ export default function App() {
 const styles = StyleSheet.create({
   wow: {
     flex: 1,
-    backgroundColor: "dodgerblue",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
   },
